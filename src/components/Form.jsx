8101c53f4da56c8456f103deafb94098314123ea
Devendragupta96 <devendrakochartech@gmail.com>
2022-08-31 10:14:59 +0530

    Updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,66 +1,143 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const Form = ({user}) => {
-  console.log(user,"form")
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.user = props.user;
+  }
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [age, setAge] = useState(0);
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    age: 0,
+  };
 
-
-         
-const addUser= async (e)=>{
+  async addUser(e) {
     e.preventDefault();
-   await fetch("http://localhost:3000/users/create",{
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({
-        name,password,email,age
-        })
-})
-   
+    await fetch("http://localhost:3000/users/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: this.state.name,
+        password: this.state.password,
+        email: this.state.email,
+        age: this.state.age,
+      }),
+    });
+  }
+  render() {
+    return (
+      <form>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="name"
+          value={this.state.name}
+          onChange={(e) =>
+            this.setState({ ...this.state, name: e.target.value })
+          }
+        />
+
+        <input
+          className="form-control"
+          type="email"
+          placeholder="email"
+          value={this.state.email}
+          onChange={(e) =>
+            this.setState({ ...this.state, email: e.target.value })
+          }
+        />
+
+        <input
+          className="form-control"
+          type="number"
+          placeholder="age"
+          value={this.state.age}
+          onChange={(e) =>
+            this.setState({ ...this.state, age: e.target.value })
+          }
+        />
+
+        <input
+          className="form-control"
+          type="password"
+          placeholder="password"
+          value={this.state.password}
+          onChange={(e) =>
+            this.setState({ ...this.state, password: e.target.value })
+          }
+        />
+
+        <button className="btn btn-primary" onClick={this.addUser}>
+          Submit
+        </button>
+
+        <div>{this.user?.email}</div>
+      </form>
+    );
+  }
 }
 
-  return (
-    <form>
-      <input
-        className="form-control"
-        type="text"
-        placeholder="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+// const Form = ({user}) => {
+//   console.log(user,"form")
 
-      <input
-              className="form-control"
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [age, setAge] = useState(0);
 
-      <input
-              className="form-control"
-        type="number"
-        placeholder="age"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
+// const addUser= async (e)=>{
+//     e.preventDefault();
+//    await fetch("http://localhost:3000/users/create",{
+//     method:'POST',
+//     headers:{'Content-Type':'application/json'},
+//     body:JSON.stringify({
+//         name,password,email,age
+//         })
+// })
 
-      <input
-              className="form-control"
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+// }
 
-      <button className="btn btn-primary" onClick={addUser}>Submit</button>
+//   return (
+//     <form>
+//       <input
+//         className="form-control"
+//         type="text"
+//         placeholder="name"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
 
-      <div>{user?.email}</div>
-    </form>
-  );
-};
+//       <input
+//               className="form-control"
+//         type="email"
+//         placeholder="email"
+//         value={email}
+//         onChange={(e) => setEmail(e.target.value)}
+//       />
 
-export default Form;
+//       <input
+//               className="form-control"
+//         type="number"
+//         placeholder="age"
+//         value={age}
+//         onChange={(e) => setAge(e.target.value)}
+//       />
+
+//       <input
+//               className="form-control"
+//         type="password"
+//         placeholder="password"
+//         value={password}
+//         onChange={(e) => setPassword(e.target.value)}
+//       />
+
+//       <button className="btn btn-primary" onClick={addUser}>Submit</button>
+
+//       <div>{user?.email}</div>
+//     </form>
+//   );
+// };
+
+// export default Form;
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,27 +1,64 @@
-import React from 'react'
+import React from "react";
 
-const User = ({user,i}) => {
+class User extends React.Component{
+  constructor({user,i}){
+    super({user,i});
+    this.user=user;
+    this.i=i;
+    console.log(user)
+  }
+ 
+
+ async deleteUser(e) {
+  e.preventdefault();
+  await fetch(`http://localhost:3000/users/${this.user._id}`,{method:'DELETE'})
+}
+
+  render(){
+    return(
+      <div>
+            <h1>{(this.i)+1}</h1>
+            <p>{this.user?.email} </p>
+            <p>{this.user?.password} </p>
+            <p>{this.user?.name} </p>
+            <p>{this.user?.age} </p>
+            <button className="btn btn-danger" onClick={this.deleteUser}>Delete</button>
+            {/* <button className="btn btn-danger" onClick={deleteUser}>Delete</button> */}
+      
+      </div>
+    );
+
+  }
+}
+
+
+
+
+
+// import React from 'react'
+
+// const User = ({user,i}) => {
 
          
-const deleteUser= async (e)=>{
-  //e.preventDefault();
-  await fetch(`http://localhost:3000/users/${user._id}`,{method:'DELETE'})
+// const deleteUser= async (e)=>{
+//   //e.preventDefault();
+//   await fetch(`http://localhost:3000/users/${user._id}`,{method:'DELETE'})
  
-}
+// }
 
 
-  return (
-    <div>
-      <h1>{i+1}</h1>
-      <p>{user?.email} </p>
-      <p>{user?.password} </p>
-      <p>{user?.name} </p>
-      <p>{user?.age} </p>
-      <button className="btn btn-danger" onClick={deleteUser}>Delete</button>
-      {/* <button className="btn btn-danger" onClick={deleteUser}>Delete</button> */}
+//   return (
+//     <div>
+//       <h1>{i+1}</h1>
+//       <p>{user?.email} </p>
+//       <p>{user?.password} </p>
+//       <p>{user?.name} </p>
+//       <p>{user?.age} </p>
+//       <button className="btn btn-danger" onClick={deleteUser}>Delete</button>
+//       {/* <button className="btn btn-danger" onClick={deleteUser}>Delete</button> */}
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
 export default User
